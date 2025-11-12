7ef338003e23cadbdeb7cd89c5ae85c916abcf0a
Sourabh pal <palsourabh094@gmail.com>
2025-11-12 23:33:50 +0530

    Add files via upload

---
+++ Assignment 1 tutedude.py
@@ -0,0 +1,15 @@
+# Name : sourabh pal
+# course : Python
+#Assignment 1
+
+
+a = float(input("Enter the number 1 : "))
+b = float(input("Entre the number 2 : "))
+result = a+b
+print(f"Addition : {result}")
+result2 = a-b
+print(f"Subtraction : {result2}")
+result3 = a*b
+print(f"Multiplication : {result3}")
+result4 = a/b
+print(f"Division : {result4}")
