09eafa90adee2dc18a4186d0f3014a0e0ca996ea
Sourabh pal <palsourabh094@gmail.com>
2025-11-12 23:39:53 +0530

    Add Task 1 and Task 2 to tutedude.py

--- Assignment 1 tutedude.py
+++ Assignment 1 tutedude.py
@@ -2,7 +2,7 @@
 # course : Python
 #Assignment 1
 
-
+#Task 1 
 a = float(input("Enter the number 1 : "))
 b = float(input("Entre the number 2 : "))
 result = a+b
@@ -13,3 +13,8 @@ result3 = a*b
 print(f"Multiplication : {result3}")
 result4 = a/b
 print(f"Division : {result4}")
+
+#Task 2
+first_name = str(input("Enter the first name : "))
+last_name = str(input("Enter the last name : "))
+print(f"Hello, {first_name}{last_name}! welcome to the python program ")
