e65dfc3735a3214e451db4bee705f7e45e5b6bfc
Sourabh pal <palsourabh094@gmail.com>
2026-01-03 01:37:12 +0530

    Add files via upload

---
+++ Assignment 2  py.py
@@ -0,0 +1,20 @@
+#Name : sourabh pal
+#course : Python
+# Assignment 2
+# Task 1
+# Check if a Number is Even or Odd
+num = int(input("Enter a number : "))
+if num%2 == 0 :
+       print("The given number is even ")
+else :
+       print("The given number is odd ")
+
+# Task 2
+# The sum of 1 to 50 by Using by the for loop
+n = 50
+sum_ = 0
+for i in range(1,n+1):
+       sum_ += i
+       print(sum_)
+print("The sum of the number from 1 to 50 is : ",sum_)
+
